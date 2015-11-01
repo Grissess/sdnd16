7104753cc32d4e058edcb8cb6fe30990b57346d8
Killian Coddington <coddinkn@users.noreply.github.com>
2015-11-01 18:42:10 -0500

    documented database code, dont really get godoc yet but its a start

--- database/model.go
+++ database/model.go
@@ -4,6 +4,7 @@
  * David Josephs and Killian Coddington
  */
 
+// A package developed to ease interaction between topology information and a redis database.
 package database
 
 import (
@@ -11,21 +12,25 @@ import (
 	"github.com/garyburd/redigo/redis"
 )
 
+// Data structure to hold information about the paths between nodes in a topology.
 type PathsData struct {
 	paths []string
 	numNodes int
 }
 
+// Create a new PathsData structure with empty path information and a specified number of nodes.
 func NewPathsData(numberOfNodes int) PathsData {
 	return PathsData{paths: make([]string, numberOfNodes * numberOfNodes), numNodes: numberOfNodes}
 }
 
+// Set a specific path from source to destination nodes.
 func (self *PathsData) SetPath(source int, destination int, path string) {
 	if source < self.numNodes && destination < self.numNodes && source >= 0 && destination >= 0 {
 		self.paths[(source * self.numNodes) + destination] = path
 	}
 }
 
+// Get a specific path between source and destination nodes.
 func (self *PathsData) GetPath(source int, destination int) string {
 	if source < self.numNodes && destination < self.numNodes && source >= 0 && destination >= 0 {
 		return self.paths[(source * self.numNodes) + destination]
@@ -34,6 +39,7 @@ func (self *PathsData) GetPath(source int, destination int) string {
 	}
 }
 
+// Return the number of nodes in a given topology PathsData is storing paths for.
 func (self *PathsData) GetNumNodes() int {
 	return self.numNodes
 }
@@ -63,6 +69,8 @@ func (self *DsTopologyData) GetNodeNeighbors(node int) string {
 }
 */
 
+// Structure devoted to storing a PathsData for a topology, as well as a connection to a redis database.
+// Functions RoutingDatabase provides aim to ease storing a set of paths or a topology in a redis database.
 type RoutingDatabase struct {
 	name string
 	connection redis.Conn
@@ -72,18 +80,22 @@ type RoutingDatabase struct {
 	numNodes int
 }
 
+// Create a new routing database structure with a given name and number of nodes.
 func NewRoutingDatabase(dbName string, numberOfNodes int) RoutingDatabase {
 	return RoutingDatabase{name: dbName, connection: nil, paths: NewPathsData(numberOfNodes), /*topology: NewTopologyData(numberOfNodes),*/ connectionInitialized: false, numNodes: numberOfNodes}
 }
 
+// Set a path in the RoutingDatabase's corresponding PathsData structure. (local)
 func (self *RoutingDatabase) SetPath(source int, destination int, path string) {
 	self.paths.SetPath(source, destination, path)
 }
 
+// Get a path from a RoutingDatabase's corresponding PathsData structure. (local)
 func (self *RoutingDatabase) GetPath(source int, destination int) string {
 	return self.paths.GetPath(source, destination)
 }
 
+// Connect to a redis database specified by a protocol (network) and address.
 func (self *RoutingDatabase) Connect(network string, address string) {
 	db, err := redis.Dial(network, address)
 	if err != nil {
@@ -95,10 +107,12 @@ func (self *RoutingDatabase) Connect(network string, address string) {
 	self.connectionInitialized = true
 }
 
+// Get the number of nodes for the topology a RoutingDatabase respresents.
 func (self *RoutingDatabase) GetNumNodes() int {
 	return self.numNodes
 }
 
+// Get path information for a specific path from a redis database. (remote) 
 func (self *RoutingDatabase) DBGetPath(source int, destination int) string {
 	if !self.connectionInitialized {
 		panic("attempting to request from uninitialized database")
@@ -110,6 +124,7 @@ func (self *RoutingDatabase) DBGetPath(source int, destination int) string {
 	return path
 }
 
+// Store all local PathsData information to a redis database. (remote)
 func (self *RoutingDatabase) StorePathsData() {
 	for i := 0; i < self.numNodes; i++ {
 		for j := 0; j < self.numNodes; j++ {

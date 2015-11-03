bf394ef34b0e231e57775d7b1bbb08f5a8cc781a
Michael Fulton <emu4hire@gmail.com>
2015-11-02 21:01:55 -0500

    filereader updated

--- reader/graph_reader.go
+++ reader/graph_reader.go
@@ -1,59 +1,50 @@
-package main
+package reader
 
 import(
 	"bufio"
 	"fmt"
-	"strconv"
 	"os"
 	"github.com/Grissess/sdnd16/network"
 )
 
 
-func main(){
+func ReadFile(filename string) network.DsGraph{
 
-	f, _ := os.Open("topology.txt")
+	f, _ := os.Open(filename)
 
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanWords)
-	var result []int
+	scanner := bufio.NewScanner(f);
+	scanner.Split(bufio.ScanWords);
+	var result []string;
 
 	for scanner.Scan(){
-		x, err:= strconv.Atoi(scanner.Text())
-		if err != nil{
-			fmt.Println(err)
-		}
-		result = append(result, x)
+		x:= scanner.Text()
+		result = append(result, x);
 	}
 
-
-	var srcs, dests, weights []int
+	var srcs, dests, weights []string
 	var i int
 
 	for i =0 ; i < len(result); i= i+3{
-		srcs = append(srcs,result[i])
+		srcs = append(srcs,result[i]);
 	}
 	for i =1; i < len(result); i= i+3{
-		dests = append(dests, result[i])
+		dests = append(dests, result[i]);
 	}
 
 	for i = 2; i< len(result); i = i+3{
-		weights = append(weights, result[i])
+		weights = append(weights, result[i]);
 	}
 
-	g := network.NewGraph()
+	g := network.NewGraph();
 
 	for i = 0; i<len(srcs); i= i +1{
 		s := g.GetOrCreateNode(network.Label(fmt.Sprintf("%d", srcs[i])))
 		d := g.GetOrCreateNode(network.Label(fmt.Sprintf("%d", dests[i])))
-		e1, _ := g.NewEdge(s,d)
-		e2, _ := g.NewEdge(d,s)
+		e1, _ := g.NewEdge(s,d);
+		e2, _ := g.NewEdge(d,s);
 		e1.SetAttr("cost", weights[i]);
 		e2.SetAttr("cost", weights[i]);
 	}
-	fmt.Println(g.ToDot());
 
-	randnode := g.GetAllNodes()[0];
-	stree, _ := g.Search(randnode, func(edge *network.DsEdge) int {return edge.GetAttr("cost").(int);});
-	fmt.Println(stree.ToDot());
-
+        return g;
 }

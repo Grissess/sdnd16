20e474a727b3566de869e668ae1d536c45ef6586
Michael Fulton <emu4hire@gmail.com>
2015-11-03 23:42:03 -0500

    test case changes

--- tests/readerTest.go
+++ tests/readerTest.go
@@ -9,12 +9,9 @@ import(
 func main(){
         var g *graph.DefaultGraph
         g = reader.ReadFile("topology.txt")
-//        fmt.Print(g.String())
+        fmt.Print(g.String())
 
-        mapy :=reader.LabelList(g);
-        fmt.Println(mapy)
-
-/*        path, distance, _:= graph.Dijkstra(g, "1", "5")
+        path, distance, _:= graph.Dijkstra(g, "1", "5")
         fmt.Println(path)
-        fmt.Println(distance) */
+        fmt.Println(distance) 
 }

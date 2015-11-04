180052fd240b269c54fe67edd44d8605b4e5020a
Michael Fulton <emu4hire@gmail.com>
2015-11-03 23:08:54 -0500

    readerTest update

--- tests/readerTest.go
+++ tests/readerTest.go
@@ -6,9 +6,18 @@ import(
         "github.com/Grissess/sdnd16/reader"
 )
 
-
 func main(){
         var g *graph.DefaultGraph
-        g = reader.ReadFile("topology.txt") 
+        g = reader.ReadFile("topology.txt")
         fmt.Print(g.String())
+
+        vertices := g.GetVertices()
+        labels := make([]string, 0, len(vertices))
+        for key := range vertices{
+                labels = append(labels, key);
+        }
+
+        path, distance, _:= graph.Dijkstra(g, "1", "5")
+        fmt.Println(path)
+        fmt.Println(distance)
 }

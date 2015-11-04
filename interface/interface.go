c9976a55b58781c446810209728996b984a6b582
josephd <josephd@clarkson.edu>
2015-11-03 19:46:26 -0500

    updated interface

--- interface/interface.go
+++ interface/interface.go
@@ -31,7 +31,8 @@ func CreateDataBase(filename string, name string, address string){
 }
 
 func main() {
-
+	
+	done := 0
     var filename string
     var address string
     var name string
@@ -61,6 +62,22 @@ func main() {
 		CreateDataBase(os.Args[1], os.Args[2], os.Args[3])	
 	}else{
 		fmt.Println("Invalid input program Terminated")	
+		done = 1
 	}
-
+	var answer string
+	for ! (done == 0) {
+		var start int
+		var end int
+		fmt.Print("what is your starting node? > ")
+		fmt.Scanln(&start)
+		fmt.Print("what is your ending node? > ")
+		fmt.Scanln(&end)
+		fmt.Println("first path is")
+		fmt.Println("second path is")
+		fmt.Print("enter q to quit")
+		fmt.Scanln(&answer)
+		if answer == "q" {
+			done = 0
+		}
+	}
 }

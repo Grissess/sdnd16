c5bfa55602b6f5cbbe87e46674497df2ba8d8737
Michael Fulton <emu4hire@gmail.com>
2015-12-02 02:44:25 -0500

    Added colors and formatted output

--- client/client2.go
+++ client/client2.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Grissess/sdnd16/utils"
         "github.com/Grissess/sdnd16/algorithms"
         "github.com/gonum/graph/path"
+        "github.com/fatih/color"
 )
 
 func main() {
@@ -15,18 +16,31 @@ func main() {
         var db database.RoutingDatabase
         var DBerr error
 
-        fmt.Println("Welcome to the NRA System, written in golang")
+        color.Set(color.FgHiGreen, color.Bold, color.BlinkSlow)
+        fmt.Println("\nWelcome to the NRA System, written in golang\n")
+        color.Unset()
+
+
         fmt.Println("Enter IP address and port of the database server you'd like to use")
         fmt.Println("If none is entered, the default server will be used (testing only)\n")
+        color.Unset()
+
+        color.Set(color.FgWhite)
         fmt.Print("IP address and port >> ")
+        color.Unset()
+
         fmt.Scanln(&ipAddress)
 
         if ipAddress == "" {
+                color.Set(color.FgHiBlue)
                 fmt.Println("No ip address selected, using default database")
+                color.Unset()
                 ipAddress = "128.153.144.171:6379"
         }
 
+        color.Set(color.FgWhite)
         fmt.Print("Enter the name of the database record you wish to use >> ")
+        color.Unset()
         fmt.Scanln(&recordName)
 
         exists, DBerr :=  database.DatabaseExists(recordName, "tcp", ipAddress)
@@ -36,7 +50,10 @@ func main() {
         }
 
         if exists {
-                fmt.Println("This record exists!")
+                color.Set(color.FgHiBlue)
+                fmt.Println("This record exists!\n")
+                color.Unset()
+
                 db, DBerr = database.ConnectToDatabase(recordName, "tcp", ipAddress)
 
                 if DBerr != nil {
@@ -45,12 +62,18 @@ func main() {
                 queryPaths(db)
 
        } else {
+                color.Set(color.FgHiBlue)
                 fmt.Println("This record does not exist.  Please check your spelling")
+                color.Set(color.FgWhite)
                 fmt.Print("Would you like to create a record by this name? [Y/N] >> ")
+                color.Unset()
                 fmt.Scanln(&input)
 
                 if input ==  "Y" || input == "y" {
+                        color.Set(color.FgWhite)
                         fmt.Print("Give me the name of a topology file >> ")
+                        color.Unset() 
+
                         fmt.Scanln(&filename)
                         g, labels, Uerror := utils.ReadFileToGraph(filename)
 
@@ -76,15 +99,20 @@ func main() {
 
                         db, DBerr = database.NewRoutingDatabase(recordName, "tcp", ipAddress, revLabels, realPathMap)
 
-                        if DBerr != nil {
+                         if DBerr != nil {
                                 panic(DBerr)
                         }
+
+                        color.Set(color.FgHiGreen, color.Bold)
+                        fmt.Println("Database created correctly!")
+                        color.Unset()
 
                         queryPaths(db)
 
                 } else {
-                        fmt.Println("Very well.")
+                        color.Set(color.FgHiGreen, color.Bold)
                         fmt.Println("Thank you for using golang NRA!")
+                        color.Unset()
                 }
         }
 }
@@ -93,22 +121,37 @@ func queryPaths(db database.RoutingDatabase) {
         var input, src, dst string
 
         for {
+                color.Set(color.FgWhite)
                 fmt.Print("Get node paths? [Y/N] >> ")
+                color.Unset()
                 fmt.Scanln(&input)
                 if input == "y" || input == "Y" {
+                        color.Set(color.FgWhite)
                         fmt.Print("Enter a first node >> ")
+                        color.Unset()
                         fmt.Scanln(&src)
+
+                        color.Set(color.FgWhite)
                         fmt.Print("Enter a second node >>")
+                        color.Unset()
                         fmt.Scanln(&dst)
+
                         path, DBerr := db.GetPath(src, dst)
 
                         if DBerr != nil {
-                                fmt.Println(DBerr)
+                                color.Set(color.FgHiRed, color.Bold)
+                                fmt.Println(DBerr, "\n")
+                                color.Unset()
                         } else {
-                        fmt.Println("The shortest path is: ", path)
+                        color.Set(color.FgGreen, color.Bold)
+                        fmt.Println("The shortest path is: ", path, "\n")
+                        color.Unset()
+
                         }
                  }else {
+                                color.Set(color.FgHiGreen, color.Bold)
                                 fmt.Println("Thank you for using golang NRA!")
+                                color.Unset()
                                 break
                  }
         }
